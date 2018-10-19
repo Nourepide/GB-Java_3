9595dd4e77c640e9899d84cb9eae39ad1e01d255
Nourepide <nourepide@gmail.com>
2018-10-19 05:31:01 +0400

    Rework history to chat && Refactor

--- src/main/java/net/nourepide/learning/three/Main.java
+++ src/main/java/net/nourepide/learning/three/Main.java
@@ -3,13 +3,14 @@ package net.nourepide.learning.three;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final String ADDRESS = "/home/nourepide/test.txt";
+    private static final String ADDRESS = "/home/username/file.txt";
 
     private static String name;
     private static String history;
@@ -30,29 +31,45 @@ public class Main {
         // Read history file and save it
         history = Files.readString(Paths.get(ADDRESS));
 
-        // Reverse and enter to the chat first 100 lines
-        final List<String> strings = Files.readAllLines(Paths.get(ADDRESS));
-        Collections.reverse(strings);
+        // Enter to the chat last 100 lines
+        final List<String> strings = history.lines().collect(Collectors.toList());
 
-        int iterator = (strings.size() < 100) ? strings.size() -1 : 100;
+        // If size lesser than 100 - start from 0
+        // If size larger than 100 - start from size - 100
+        final int start = strings.size() < 100 ? 0 : strings.size() - 100;
+        final int end = strings.size();
 
-        for (; iterator >= 0 ; iterator--) {
-            System.out.println(strings.get(iterator));
-        }
+        IntStream.range(start, end)
+                .mapToObj(strings::get)
+                .forEach(System.out::println);
 
-        // history.lines().limit(100).forEach(System.out::println);
-        //                 ~~~~^~~~~
-
+        // Initialisation complete
         isEnabled = true;
     }
 
+    // Returns user name
     private static String getName() {
         System.out.println("Please enter your name");
         return getLine();
     }
 
+    // Returns user input
     private static String getLine() {
         return scanner.nextLine();
+    }
+
+    // Write message to chat and file
+    private static void writeMessage() throws IOException {
+        String message = getLine();
+
+        // Stop application if user wrote exit
+        if (message.equals("exit")) {
+            isEnabled = false;
+            return;
+        }
+
+        writeMessageToChat(name, message);
+        writeMessageToFile(name, message);
     }
 
     private static void writeMessageToChat(String name, String message) {
@@ -62,17 +79,5 @@ public class Main {
     private static void writeMessageToFile(String name, String message) throws IOException {
         history = history + name + ": " + message + System.lineSeparator();
         Files.writeString(Paths.get(ADDRESS), history);
-    }
-
-    private static void writeMessage() throws IOException {
-        String message = getLine();
-
-        if (message.equals("exit")) {
-            isEnabled = false;
-            return;
-        }
-
-        writeMessageToChat(name, message);
-        writeMessageToFile(name, message);
     }
 }
