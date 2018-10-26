568dfff5d724fc8c046cf841d1ba68853b446633
Nourepide <nourepide@gmail.com>
2018-10-27 02:00:36 +0400

    Change ArrayList to List

--- src/main/java/net/nourepide/learning/five/Race.java
+++ src/main/java/net/nourepide/learning/five/Race.java
@@ -4,15 +4,16 @@ import net.nourepide.learning.five.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Race {
-    private final ArrayList<Stage> stages;
+    private final List<Stage> stages;
 
     public Race(Stage... stages) {
         this.stages = new ArrayList<>(Arrays.asList(stages));
     }
 
-    public ArrayList<Stage> getStages() {
+    public List<Stage> getStages() {
         return stages;
     }
 }
