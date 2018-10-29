a4c081513a0c89a960285cbe5ac949ed2bd22f35
Nourepide <nourepide@gmail.com>
2018-10-29 15:27:07 +0400

    Unwrap blocks

--- src/test/java/net/nourepide/learning/ApplicationTest.java
+++ src/test/java/net/nourepide/learning/ApplicationTest.java
@@ -8,47 +8,37 @@ public class ApplicationTest {
 
     @Test
     public void checkArray() {
-        {
-            boolean result = Application.checkArray(new int[]{1, 4}, 1, 4);
-            Assertions.assertTrue(result);
-        }
+        Assertions.assertTrue(
+                Application.checkArray(new int[]{1, 4}, 1, 4)
+        );
 
-        {
-            boolean result = Application.checkArray(new int[]{4, 4}, 1, 4);
-            Assertions.assertFalse(result);
-        }
+        Assertions.assertFalse(
+                Application.checkArray(new int[]{4, 4}, 1, 4)
+        );
 
-        {
-            boolean result = Application.checkArray(new int[]{1, 1}, 1, 4);
-            Assertions.assertFalse(result);
-        }
+        Assertions.assertFalse(
+                Application.checkArray(new int[]{1, 1}, 1, 4)
+        );
 
-        {
-            boolean result = Application.checkArray(new int[]{0, 0}, 1, 4);
-            Assertions.assertFalse(result);
-        }
+        Assertions.assertFalse(
+                Application.checkArray(new int[]{0, 0}, 1, 4)
+        );
     }
 
     @Test()
     public void changeArray() {
-        {
-            Assertions.assertThrows(RuntimeException.class, () ->
-                    Application.changeArray(new int[]{}, 4)
-            );
-        }
+        Assertions.assertThrows(RuntimeException.class, () ->
+                Application.changeArray(new int[]{}, 4)
+        );
 
-        {
-            Assertions.assertThrows(RuntimeException.class, () -> {
-                int[] array = {1, 2, 0, 0, 2, 3, 0, 1, 7};
-                Application.changeArray(array, 4);
-            });
-        }
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            int[] array = {1, 2, 0, 0, 2, 3, 0, 1, 7};
+            Application.changeArray(array, 4);
+        });
 
-        {
-            int[] array = {1, 2, 4, 4, 2, 3, 4, 1, 7};
-            int[] result = Application.changeArray(array, 4);
+        int[] array = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+        int[] result = Application.changeArray(array, 4);
 
-            Assertions.assertArrayEquals(new int[]{1, 7}, result);
-        }
+        Assertions.assertArrayEquals(new int[]{1, 7}, result);
     }
 }
